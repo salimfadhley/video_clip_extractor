76c4911da4dd3851e7037ba1553ddd0f8fa60001
Salim Fadhley <salimfadhley@gmail.com>
2020-11-30 22:56:59 +0000

    my cool clipping tool

---
+++ src/cliplist.py
@@ -0,0 +1,57 @@
+import logging
+from src.source import project_maker
+
+
+def main():
+    with project_maker("mark_steele") as p:
+        with p.clip_maker("1.mp4") as s:
+            s.clip("education1.mp4", "00:20:25", "0:21:33", title="education 1")
+            s.clip("education2.mp4", "00:23:28", "0:24:00", title="Why where you chosen for the job")
+            s.clip("where_did_life_take_you.mp4", "0:24:00", "00:29:59", title="Mark's book voucher")
+            s.clip("interest_in_weapons.mp4", "00:30:09", "0:30:50", title="He knew a lot about weapons for some inexplicable reason")
+            s.clip("interrogating.mp4", "00:30:58", "0:31:22", title="He used to interrogate scientists")
+            s.clip("5g_activates_pathogen.mp4", "00:35:55", "0:36:26", title="Mark links 5G to the current pandemic")
+            s.clip("fcc_reg1.mp4", "00:37:45", "0:38:36", title="Mark thinks that the Leafnut device is not FCC approved")
+            s.clip("fcc_reg2.mp4", "00:39:28", "0:39:52", title="Mark thinks the FCC registration was forged")
+            s.clip("fcc_reg3.mp4", "00:40:30", "0:41:28", title="Mark rants more about the FCC")
+            s.clip("dipole_antenna.mp4", "00:41:43", "0:42:03", title="Toon held a dipole antenna")
+            s.clip("leafnut.mp4", "00:42:09", "0:43:35", title="Mark thinks it's impossible to buy a LeafNut")
+            s.clip("challenge.mp4", "00:46:40", "0:47:02", title="Mark's challenge to McToon")
+            s.clip("what_is_a_high_gain_antenna.mp4", "00:51:16", "0:52:11", title="I ask Mark what is meant by a high-gain antenna?")
+            s.clip("what_is_a_monopole.mp4", "00:53:29", "0:53:58", title="Mark says that a monpole isn't a kind of antenna")
+            s.clip("what_is_a_high_gain_antenna.mp4", "00:54:01", "0:54:27", title="Mark defines a high-gain antenna")
+            s.clip("that_blob.mp4", "00:56:53", "0:57:28", title="Mark thinks that varnishing an antenna would stop it working")
+            s.clip("lenses.mp4", "01:03:41", "1:04:46", title="Mark explains how lenses work")
+            s.clip("the_main_technical_parameter_for_5g_0.mp4", "01:06:43", "1:07:11", title="Mark claims that the main technical parameter for 5G is focussed energy in air")
+            s.clip("the_main_technical_parameter_for_5g_1.mp4", "01:07:34", "1:08:05", title="Mark talks a bit more about technical parameters")
+            s.clip("if_5g_just_a_weapon.mp4", "01:09:38", "1:09:54", title="Mark syas that 5g 5g phones don't exist")
+            s.clip("if_5g_just_a_weapon.mp4", "01:10:40", "1:11:45", title="Mark explains why 5g phones cannot exist")
+            s.clip("how_does_it_kill.mp4", "01:13:34", "1:13:52", title="Ionization is lethal!")
+            s.clip("what_is_the_effect_on_my_Body.mp4", "01:13:34", "1:14:16", title="Mark says that 5G can vaporize you!")
+            s.clip("9-11_0.mp4", "01:15:10", "1:15:25", title="Mark says that the destruction of the twin towers was caused by 5g!")
+            s.clip("9-11_1.mp4", "01:17:16", "1:18:09", title="Mark talks more about how 5g destroyed the twin towers!")
+        with p.clip_maker("2.mp4") as s:
+            s.clip("toons_agenda.mp4", "00:04:24", "00:05:11", title="Mark says he enjoyed taking the piss out of McToon")
+            s.clip("who_is_mctoon.mp4", "00:06:16", "00:06:30", title="Mark says McToon is taking him out of context")
+            s.clip("toons_agenda.mp4", "00:04:24", "00:05:11", title="Mark says he enjoyed taking the piss out of McToon")
+            s.clip("toons_organization.mp4", "00:06:46", "00:07:07", title="Mark suggests that Toon might be connected")
+            s.clip("cabal.mp4", "00:09:29", "00:09:42", title="Mark talks about the Cabal")
+            s.clip("prophecy.mp4", "00:11:09", "00:12:31", title="Mark says that he has evidence that five cities were destroyed in Korea")
+            s.clip("what_other_products.mp4", "00:17:03", "00:17:47", title="I asked Mark what other products her worked on")
+            s.clip("marks_impossible_carbon_cracker.mp4", "00:20:58", "00:21:42", title="Mark claims that he invented a machine that could break down carbon dioxide")
+            s.clip("spectrum_analyser.mp4", "00:25:36", "00:26:47", title="Mark claims that he measured the output of the harvard leafnode with a spectrum analyzer")
+            s.clip("non_ionizing_radiation.mp4", "00:25:36", "00:30:40", title="Mark defines non-ionizing radiation")
+            s.clip("ionization.mp4", "00:30:42", "00:31:05", title="Mark defines what an ion is")
+            s.clip("ionization.mp4", "00:31:19", "00:32:29", title="Mark is confused about radiation")
+            s.clip("ionization.mp4", "00:32:29", "00:32:58", title="Mark is confused about radiation")
+            s.clip("ionization.mp4", "00:32:58", "00:34:40", title="Mark is confused about ionization")
+            s.clip("why_are_5g_phones_faster.mp4", "00:36:03", "00:36:53", title="Mark does not believe that 5g phones exist")
+            s.clip("how_does_5g_kill.mp4", "00:38:36", "00:39:18", title="Mark thinks 1kw would cause you to die")
+            s.clip("inverse_square_law.mp4", "00:40:13", "00:40:57", title="Mark explains why the inverse square law does not apply to 5g")
+            s.clip("5g_targeting.mp4", "00:43:22", "00:43:53", title="Mark Mark explains how the targeting works")
+            s.clip("5g_is_not_targeted.mp4", "00:44:23", "00:45:20", title="Lasers in the jungle?")
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger("").setLevel(logging.INFO)
+    main()
---
+++ src/source.py
@@ -0,0 +1,91 @@
+import re
+import os
+from contextlib import contextmanager
+from dataclasses import dataclass, field
+from typing import List, Mapping
+import logging
+import subprocess
+
+log = logging.getLogger(__name__)
+
+@dataclass
+class Source:
+    project: "Project"
+    file_path: str
+
+
+    def clip(self, output_filename:str, start_timecode: str, end_timecode: str, **metadata) -> "Clip":
+        c: Clip = Clip(self, output_filename=output_filename, start_timecode=start_timecode, end_timecode=end_timecode, metadata=metadata)
+        c.validate()
+        self.project.clips.append(c)
+        return c
+
+
+def timestamp_to_seconds(timestamp:str)->int:
+    h,m,s = [int(x) for x in timestamp.split(":")]
+    return s + m * 60 + h * 3600
+
+@dataclass
+class Clip:
+    source: Source
+    output_filename: str
+    start_timecode: str
+    end_timecode: str
+    metadata: Mapping[str,str]
+
+    def validate(self):
+        assert timestamp_to_seconds(self.start_timecode) < timestamp_to_seconds(self.end_timecode), f"This clip is invalid because it ends before it starts: {self.output_filename}"
+        assert not (" " in self.output_filename), f"Output filename should not contain any whitespace: {self.output_filename}"
+        assert self.output_filename.endswith(".mp4"), f"invalid filename: {self.output_filename}."
+        return True
+
+    def extract(self, index_number:int, output_directory:str):
+        assert os.path.exists(output_directory) and os.path.isdir(output_directory), f"{output_directory} should be a directory"
+
+        full_filename = f"{self.source.project.project_name}.{index_number}.{self.output_filename}"
+        output_file_path = os.path.join(output_directory, full_filename)
+
+        if not os.path.exists(output_file_path):
+            log.info(f"Extracting {self}")
+            metadata_string = " ".join([f"-metadata {k}=\"{v}\"" for (k,v) in self.metadata.items()])
+            command = ["ffmpeg", "-i", self.source.file_path, "-ss", self.start_timecode, "-to", self.end_timecode, "-async", "1",  metadata_string, output_file_path]
+            log.info(f"Executing: {' '.join(command)}")
+            output = subprocess.check_output(" ".join(command), shell=True)
+            log.info(output)
+
+
+
+def get_input_directory()->str:
+    return os.environ["INPUT_DIRECTORY"]
+
+
+def get_output_directory()->str:
+    return os.environ["OUTPUT_DIRECTORY"]
+
+
+
+@dataclass()
+class Project:
+    project_name: str
+    clips: List["Clip"] = field(default_factory=list)
+
+    @contextmanager
+    def clip_maker(self, filename: str):
+        file_path = os.path.join(get_input_directory(), filename)
+        assert os.path.exists(file_path), f"Input file {file_path} does not exist."
+        s = Source(project=self, file_path=file_path)
+        yield s
+
+        for c in self.clips:
+            c.validate()
+
+        for i, clip in enumerate(self.clips):
+            clip.extract(output_directory=get_output_directory(), index_number=i)
+
+
+@contextmanager
+def project_maker(project_name:str):
+    project = Project(project_name=project_name)
+    yield project
+
+
